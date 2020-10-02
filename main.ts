b51a4e749aa3e7963099d16433090f3dc3c2d47c
Ahmed-Ali-0 <71400128+Ahmed-Ali-0@users.noreply.github.com>
2020-10-02 11:34:51 -0400

    Add program

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+// Created by: Ahmed Ali
+// Created date : Oct2, 2020
+// 
+// This program used Sonar
+let distance_to_object = 0
+basic.showIcon(IconNames.Yes)
+basic.pause(500)
 basic.forever(function () {
-	
+    distance_to_object = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+    basic.showNumber(distance_to_object)
 })
